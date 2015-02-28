efb9c933a85a9b410ebb0e435f95fdf0fdf435bb
K.Popov <Yumofsc@gmail.com>
2015-02-28 13:24:06 +0200

    music

---
+++ TeamArchMageGame/KiwisMatrix/music/GameMusic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music
+{
+    class GameMusic
+    {
+        static void Main()
+        {
+            //(new SoundPlayer(@"C:\Users\Yumo\Desktop\kiviwav.wav")).PlayLooping();
+            //(new SoundPlayer(@"C:\Users\Yumo\Desktop\kiviwav.wav")).SoundLocation(C:\Users\Yumo\Desktop\kiviwav.wav);
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = @"C:\Users\Yumo\Desktop\kiviwav.wav";
+            player.PlaySync();
+            player.PlayLooping();
+        }
+    }
+}
